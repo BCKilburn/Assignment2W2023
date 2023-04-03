1a3d5e2ff83888b0e030022ad3e1e8c0f7e980f4
BCKilburn <122587442+BCKilburn@users.noreply.github.com>
2023-04-02 18:21:23 -0700

    Book class

---
+++ src/main/java/com/example/assignment2w2023/Book.java
@@ -0,0 +1,50 @@
+package com.example.assignment2w2023;
+
+import javafx.scene.image.Image;
+
+public class Book {
+    private Image image;
+
+    protected String title, author;
+
+    protected double price;
+
+    public Book(Image image, String title, String author, double price) {
+        this.image = image;
+        this.title = title;
+        this.author = author;
+        this.price = price;
+    }
+
+    public Image getImage() {
+        return image;
+    }
+
+    public void setImage(Image image) {
+        this.image = image;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
--- src/main/java/com/example/assignment2w2023/LibraryController.java
+++ src/main/java/com/example/assignment2w2023/LibraryController.java
@@ -3,7 +3,7 @@ package com.example.assignment2w2023;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class LibraryController {
     @FXML
     private Label welcomeText;
 
